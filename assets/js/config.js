b930ee560d2880e5308dca9c3d4f179b6b39085f
Lior Chamla <lchamla@gmail.com>
2019-06-04 18:40:43 +0200

    last change

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,9 @@
 const apiUrl = process.env.API_URL;
 
 const endPoints = {
-  customers: apiUrl + "customers/",
-  invoices: apiUrl + "invoices/",
-  users: apiUrl + "users/",
+  customers: apiUrl + "customers",
+  invoices: apiUrl + "invoices",
+  users: apiUrl + "users",
   login: apiUrl + "login_check"
 };
 
